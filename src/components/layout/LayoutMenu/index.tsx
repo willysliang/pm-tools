1fbf2a11f3875cf8b3ffa056cb8c7231d60390fb
willy <willysliang@qq.com>
2024-10-29 17:05:43 +0800

    fix(layout): 修复菜单栏中活跃菜单的判断问题

--- src/components/layout/LayoutMenu/index.tsx
+++ src/components/layout/LayoutMenu/index.tsx
@@ -2,7 +2,7 @@
  * @ Author: willy
  * @ CreateTime: 2024-06-20 16:38:37
  * @ Modifier: willysliang
- * @ ModifierTime: 2024-10-24 16:06:28
+ * @ ModifierTime: 2024-10-29 17:03:18
  * @ Description: 菜单栏
  */
 
@@ -17,11 +17,22 @@ export const LayoutMenu = (): ReactElement => {
   const { menuList, activeMenu, setMenu } = useAppStore();
 
   const navigate = useNavigate();
+
+  /** 选择菜单项 */
   const handleSelectMenu = (menu: IRouteConfig, event?: React.MouseEvent<HTMLElement>) => {
     if (event) event.preventDefault();
 
     setMenu(menu);
     navigate(menu.path);
+  };
+
+  /**
+   * @function isActiveMenu 当前菜单项是否活跃
+   * @description 如果跟活跃的路由的key值相同，或者活跃的路由 path 是菜单路由的子路由，则认为该菜单项是活跃的
+   */
+  const isActiveMenu = (menu: IRouteConfig) => {
+    const isActive = activeMenu.key === menu.key || activeMenu.path.includes(menu.path + '/');
+    return isActive;
   };
 
   return (
@@ -35,19 +46,19 @@ export const LayoutMenu = (): ReactElement => {
         <img src={reactLogo} className='logo animation__spin' alt='React logo' />
       </a>
 
-      {menuList.map((item) => (
+      {menuList.map((menu) => (
         <div
-          className={`layout-menu__item ${item.key === activeMenu.key ? 'layout-menu__item--active' : ''}`}
-          key={item.key}
-          title={item.label}
-          onClick={() => handleSelectMenu(item)}
+          className={`layout-menu__item ${isActiveMenu(menu) ? 'layout-menu__item--active' : ''}`}
+          key={menu.key}
+          title={menu.label}
+          onClick={() => handleSelectMenu(menu)}
         >
           <IconPark
-            icon={item.icon}
+            icon={menu.icon}
             size={22}
             theme='filled'
             style={{
-              color: `var(${item.key === activeMenu.key ? '--theme-active-color' : '--theme-normal-color'})`,
+              color: `var(${isActiveMenu(menu) ? '--theme-active-color' : '--theme-normal-color'})`,
             }}
           />
         </div>
